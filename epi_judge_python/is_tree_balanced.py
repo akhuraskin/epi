42a9ad3a3149e50279de1ad3a3ca6ad54210ddf5
Alexey Khuraskin <alexey.khuraskin@woven-planet.global>
2024-10-25 17:12:39 -0700

    9.1 is tree balanced

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,9 +1,21 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+def height(n: BinaryTreeNode) -> int:
+    if not n:
+        return 0
+    else:
+        hl = height(n.left)
+        hr = height(n.right)
+        if abs(hr-hl) > 1:
+            raise Exception
+        return max(hl, hr) + 1
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
+    try:
+        height(tree)
+    except Exception:
+        return False
     return True
 
 
