5525355b99b32c5d0eee8e20f233b4b80a128369
Alexey Khuraskin <alexey.khuraskin@woven-planet.global>
2024-10-28 16:39:04 -0700

    12.3 lru cache

--- epi_judge_python/lru_cache.py
+++ epi_judge_python/lru_cache.py
@@ -1,22 +1,33 @@
+import collections
+
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
 class LruCache:
     def __init__(self, capacity: int) -> None:
-        # TODO - you fill in here.
-        return
+        self.cache = collections.OrderedDict()
+        self.capacity = capacity
 
     def lookup(self, isbn: int) -> int:
-        # TODO - you fill in here.
-        return 0
+        if isbn not in self.cache:
+            return -1
+        self.cache.move_to_end(isbn)
+        return self.cache[isbn]
 
     def insert(self, isbn: int, price: int) -> None:
-        # TODO - you fill in here.
-        return
+        if isbn in self.cache:
+            self.cache.move_to_end(isbn)
+        else:
+            if len(self.cache) == self.capacity:
+                self.cache.popitem(last=False)
+            self.cache[isbn] = price
 
     def erase(self, isbn: int) -> bool:
-        # TODO - you fill in here.
+        try:
+            self.cache.pop(isbn)
+        except KeyError:
+            return False
         return True
 
 
