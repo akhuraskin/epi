f5c8a11b3ef9eb51a3715d9746834c7e1322ef01
Alexey Khuraskin <alexey.khuraskin@woven-planet.global>
2024-10-28 15:42:52 -0700

    11.1 search for first occurrence of k

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -4,8 +4,16 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    l, r = 0, len(A)    # k is within [l, r)
+    while l < r:
+        m = l + (r-l) // 2
+        if A[m] >= k:
+            r = m
+        else:
+            l = m+1
+    if 0 <= l < len(A) and A[l] == k:
+        return l
+    return -1
 
 
 if __name__ == '__main__':
