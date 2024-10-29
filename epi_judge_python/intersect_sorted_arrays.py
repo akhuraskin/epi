fbbf7e0845054277d902c3a2fd996a6947dc269d
Alexey Khuraskin <alexey.khuraskin@woven-planet.global>
2024-10-28 17:20:34 -0700

    13.1 intersect sorted arrays

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -4,8 +4,30 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    result = []
+
+    def unique(arr):
+        prev = None
+        for v in arr:
+            if prev is None or prev != v:
+                prev = v
+                yield v
+
+    arr_a = unique(A)
+    arr_b = unique(B)
+
+    a = next(arr_a, None)
+    b = next(arr_b, None)
+    while a is not None and b is not None:
+        if a == b:
+            result.append(a)
+            a = next(arr_a, None)
+            b = next(arr_b, None)
+        elif a < b:
+            a = next(arr_a, None)
+        elif b < a:
+            b = next(arr_b, None)
+    return result
 
 
 if __name__ == '__main__':
