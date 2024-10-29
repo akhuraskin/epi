5437818b61455f747e864e948457a487c69171f8
Alexey Khuraskin <askhuraskin@gmail.com>
2024-10-28 22:30:52 -0700

    14.1 is tree a bst

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -2,8 +2,20 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+def is_binary_tree_bst_(r: BinaryTreeNode, low=float('-inf'), high=float('inf')) -> None:
+    if not r:
+        return
+    if low <= r.data <= high:
+        is_binary_tree_bst_(r.left, low=low, high=r.data)
+        is_binary_tree_bst_(r.right, low=r.data, high=high)
+    else:
+        raise ValueError(f"Failed at {r.data}")
+
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
+    try:
+        is_binary_tree_bst_(tree)
+    except ValueError:
+        return False
     return True
 
 
