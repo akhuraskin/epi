ae9285cbe2355e60c1b8351d2abc5176791b03a8
Alexey Khuraskin <askhuraskin@gmail.com>
2024-11-02 14:58:01 -0700

    15.2 n queens

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -4,8 +4,22 @@ from test_framework import generic_test
 
 
 def n_queens(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def conflicts(row: int, col: int):
+        return any(abs(c-col) in (0, abs(row-r)) for r, c in enumerate(col_placement[:row]))
+
+    def solve_n_queens(row: int):
+        if row == n:
+            result.append(col_placement[:])
+        else:
+            for col in range(n):
+                if not conflicts(row, col):
+                    col_placement[row] = col
+                    solve_n_queens(row + 1)
+
+
+    result, col_placement = [], [0] * n
+    solve_n_queens(0)
+    return result
 
 
 def comp(a, b):
