ed37f36d804af3e7c69f488765f9041b8d73d2c6
Alexey Khuraskin <askhuraskin@gmail.com>
2024-11-08 22:18:44 -0800

    17.1 task pairing

--- epi_judge_python/task_pairing.py
+++ epi_judge_python/task_pairing.py
@@ -7,8 +7,11 @@ PairedTasks = collections.namedtuple('PairedTasks', ('task_1', 'task_2'))
 
 
 def optimum_task_assignment(task_durations: List[int]) -> List[PairedTasks]:
-    # TODO - you fill in here.
-    return []
+    task_durations = sorted(task_durations)
+    m = len(task_durations) // 2
+    tasks1, tasks2 = task_durations[:m], task_durations[m:][::-1]
+
+    return [PairedTasks(t1, t2) for t1, t2 in zip(tasks1, tasks2)]
 
 
 if __name__ == '__main__':
