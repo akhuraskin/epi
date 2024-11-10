42ee18ca59fbc1156f94a099275a84dcb4135180
Alexey Khuraskin <askhuraskin@gmail.com>
2024-11-10 14:35:37 -0800

    9.11 inorder traversal

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -3,10 +3,29 @@ from typing import List
 from binary_tree_with_parent_prototype import BinaryTreeNode
 from test_framework import generic_test
 
+def down_left(n: BinaryTreeNode) -> BinaryTreeNode:
+    while n.left is not None:
+        n = n.left
+    return n
+
+def successor(n: BinaryTreeNode) -> BinaryTreeNode:
+    if n.right:
+        return down_left(n.right)
+    else:
+        parent = n.parent
+        while parent and parent.right == n:
+            n, parent = parent, parent.parent
+        return parent
+
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    res = []
+    n = down_left(tree)
+    if n is not None:
+        res.append(n.data)
+        while n:= successor(n):
+            res.append(n.data)
+    return res
 
 
 if __name__ == '__main__':
