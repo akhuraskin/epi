73c917b597391966e2aafaf890811d2d6e6202bb
Alexey Khuraskin <askhuraskin@gmail.com>
2024-11-10 22:12:29 -0800

    19.5 online median

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -1,11 +1,25 @@
+import heapq
 from typing import Iterator, List
 
 from test_framework import generic_test
 
 
 def online_median(sequence: Iterator[int]) -> List[float]:
-    # TODO - you fill in here.
-    return []
+    res = []
+    max_heap, min_heap = [], []
+    for v in sequence:
+        heapq.heappush(min_heap, -heapq.heappushpop(max_heap, -v))
+        if len(max_heap) < len(min_heap):
+            heapq.heappush(max_heap, -heapq.heappop(min_heap))
+
+        if len(max_heap) > len(min_heap):
+            res.append(-max_heap[0])
+        elif len(max_heap) == len(min_heap):
+            res.append((-max_heap[0] + min_heap[0]) / 2)
+        else:
+            res.append(min_heap[0])
+
+    return res
 
 
 def online_median_wrapper(sequence):
