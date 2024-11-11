de1bce3103114402d01731f23da1055e947c9b08
Alexey Khuraskin <askhuraskin@gmail.com>
2024-11-10 22:26:57 -0800

    18.2 paint a boolean matrix

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -4,8 +4,18 @@ from test_framework import generic_test
 
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
-    return
+    N, M = len(image), len(image[0])
+    def neighbors(n, m, color: bool):
+        yield from ((i, j) for i, j in [(n-1, m), (n+1, m), (n, m-1), (n, m+1)]
+                    if 0<=i<N and 0<=j<M and image[i][j] == color)
+
+    src_color = image[x][y]
+    q = [(x, y)]
+    while q:
+        ci, cj = q.pop()
+        for ni, nj in neighbors(ci, cj, src_color):
+            q.append((ni, nj))
+        image[ci][cj] = not src_color
 
 
 def flip_color_wrapper(x, y, image):
