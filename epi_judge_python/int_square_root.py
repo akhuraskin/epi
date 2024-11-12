83434bed36f5f88807f6b541db0b158a49c8d0ca
Alexey Khuraskin <askhuraskin@gmail.com>
2024-11-11 22:11:08 -0800

    11.4 int square root

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,8 +2,18 @@ from test_framework import generic_test
 
 
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    l, r = 0, k
+
+    while l <= r:
+        m = l + (r-l) // 2
+        sq = m*m
+        if sq < k:
+            l = m+1
+        elif sq == k:
+            return m
+        else:   # sq > k
+            r = m - 1
+    return r
 
 
 if __name__ == '__main__':
