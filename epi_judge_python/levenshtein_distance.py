ede11c5857d064c655111afd6868a6ca952cc2af
Alexey Khuraskin <askhuraskin@gmail.com>
2025-11-08 12:53:40 -0800

    16.2 Levenstein distance

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,9 +1,22 @@
 from test_framework import generic_test
-
+import functools
 
 def levenshtein_distance(A: str, B: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    @functools.cache
+    def f(i, j):
+        if i == 0:
+            return j
+        if j == 0:
+            return i
+        if A[i-1] == B[j-1]:
+            return f(i-1, j-1)
+        return 1+min(
+            f(i-1, j-1),
+            f(i-1, j),
+            f(i, j-1)
+        )
+
+    return f(len(A), len(B))
 
 
 if __name__ == '__main__':
