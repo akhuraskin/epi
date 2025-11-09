685cf5e91d7a10f6e5515be3d6d82308ee4fed8b
Alexey Khuraskin <askhuraskin@gmail.com>
2025-11-09 14:35:10 -0800

    16.7 bed bath and beyond

--- epi_judge_python/is_string_decomposable_into_words.py
+++ epi_judge_python/is_string_decomposable_into_words.py
@@ -1,15 +1,42 @@
 import functools
-from typing import List, Set
+from typing import List, Set, Optional
 
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
-
+import functools
 
 def decompose_into_dictionary_words(domain: str,
                                     dictionary: Set[str]) -> List[str]:
-    # TODO - you fill in here.
-    return []
+
+    decomposed_suffix = {}
+    def decompose_suffix(i: int):
+        if i in decomposed_suffix:
+            return decomposed_suffix[i]
+        if i == len(domain):
+            return []
+
+        # for j in range(i+1, len(domain)+1):
+        #     w = domain[i:j]
+        #     if w in dictionary:
+        #         rest = decompose_suffix(j)
+        #         if rest is not None:
+        #             decomposed_suffix[i] = [w] + rest
+        #             return decomposed_suffix[i]
+        # decomposed_suffix[i] = None
+        # return None
+
+        for w in dictionary:
+            i_next = i + len(w)
+            if i_next <= len(domain) and domain[i:i_next] == w:
+                rest = decompose_suffix(i_next)
+                if rest is not None:
+                    decomposed_suffix[i] = [w] + rest
+                    return [w]+rest
+        decomposed_suffix[i] = None
+        return None
+
+    return decompose_suffix(0)
 
 
 @enable_executor_hook
